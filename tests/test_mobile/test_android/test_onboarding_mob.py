ef0000ad0210556182bd7f8e3bebd22037df5862
Ihar-Turtsou <ig.turtsov@gmail.com>
2025-11-10 02:57:48 +0400

    ьщиmobile test dis

--- tests/test_mobile/test_android/test_onboarding_mob.py
+++ tests/test_mobile/test_android/test_onboarding_mob.py
@@ -2,7 +2,7 @@ import allure
 import pytest
 from selene import browser
 
-
+@pytest.mark.skip(reason="This test is temporarily disabled.")
 @pytest.mark.mobile
 @pytest.mark.regression
 @allure.label("layer", "MOBILE ANDR")
